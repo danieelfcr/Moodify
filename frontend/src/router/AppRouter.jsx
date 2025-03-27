a7397c83731d39451872ebc878c0ae9746778cc0
danieelfcr <danielfcab2002@gmail.com>
2025-03-26 22:58:00 -0600

    feat: router created

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
+import { LandingPage } from '../views'
+
+
+export const AppRouter = () => {
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/home' element = {<LandingPage/>} />
+
+                <Route path='/*' element = {<LandingPage/>} />
+            </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
