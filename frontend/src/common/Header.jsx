2f67e5409fa11d3f59d0d08ac96513a64cf65bee
danieelfcr <danielfcab2002@gmail.com>
2025-03-30 21:37:04 -0600

    refactor: buttons and logo redirect to desired route

--- frontend/src/common/Header.jsx
+++ frontend/src/common/Header.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/moodify.png';
 import './Header.css';
 
 export const Header = () => {
+  const navigate = useNavigate();
   return (
     <header className="header-container">
-      <img src={logo} alt="Moodify" className="moodify-logo" />
+      <img src={logo} alt="Moodify" className="moodify-logo" onClick={() => navigate('/home')}/>
       <div className="buttons-container">
-        <button className="btn btn-login">Log in</button>
-        <button className="btn btn-signup">Sign up</button>
+        <button className="btn btn-login" onClick={() => navigate('/login')}>Log in</button>
+        <button className="btn btn-signup" onClick={() => navigate('/signup')}>Sign up</button>
       </div>
     </header>
   );
